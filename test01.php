025c4ed727ab71f317de5e8206752e822df259d2
muhamadparman <muhamad.0484@students.amikom.ac.id>
2018-10-26 09:40:43 +0700

    nyoba doank

---
+++ test01.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<fieldset>
+		<legend>Tiket Online</legend>
+
+		<form action="test02.php" method="POST" enctype="multipart/form-data">
+		<table style="width:50%">
+
+			<tr>
+				<td>Nama : </td>
+				<td><input type="text" name="nama"></td>
+			</tr>
+
+			<tr>
+				<td>PWL : </td>
+				<td><input type="text" name="pwl"></td>
+			</tr>
+
+			<tr>
+				<td>AI : </td>
+				<td><input type="text" name="ai"></td>
+			</tr>
+
+			<tr>
+				<td>SBD : </td>
+				<td><input type="text" name="sbd"></td>
+			</tr>
+
+
+		</table>
+			<input type="submit" value="Submit" name="submit">
+			
+		</form>
+
+
+	</fieldset>
+
+</body>
+</html>
